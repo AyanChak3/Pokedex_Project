341aaa8da02f99de4a0c9aff6fe6416ec97c4d22
AyanChak3 <>
2024-06-16 16:36:46 +0530

    Added pagination

--- src/Components/PokemonList/PokemonList.jsx
+++ src/Components/PokemonList/PokemonList.jsx
@@ -7,12 +7,18 @@ function PokemonList(){
     const [pokemonList,setPokemonList] = useState([]);
     const [isLoading,setIsLoading]=useState(true);
 
-    const POKEDEX_URL = "https://pokeapi.co/api/v2/pokemon"
+    const [pokedexUrl,setPokedexUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+    
+    const [nextUrl,setNextUrl] = useState('');
+    const [prevUrl,setPrevUrl] = useState('');
+    
     async function downloadPokemons(){
-       const response = await axios.get(POKEDEX_URL)//this downloads list of 20 pokemons
+       setIsLoading(true);
+       const response = await axios.get(pokedexUrl)//this downloads list of 20 pokemons
        const pokemonResults = response.data.results;//we get the array of pokemons from result
        console.log(response.data);
-
+       setNextUrl(response.data.next)
+       setPrevUrl(response.data.previous);
        //iterating over the array of pokemons and using their url,to create an array of promises
        const pokemonResultPromise = pokemonResults.map((pokemon)=> axios.get(pokemon.url));//url of 20 pokemons is stored in this array of promise
        
@@ -23,7 +29,7 @@ function PokemonList(){
        //now iterate on the data of each pokemon, and extract id,name,image,types
        const pokeListResult = pokemonData.map((pokeData)=>{ //pokemonData is the whole array of 20 pokemons
            const pokemon = pokeData.data; //we want only the data
-           return {
+           return {//object return type
             id : pokemon.id,
             name : pokemon.name,
             image : (pokemon.sprites.other) ? pokemon.sprites.other.dream_world.front_default : pokemon.sprites.front_shiny,
@@ -37,7 +43,7 @@ function PokemonList(){
 
     useEffect(()=>{
        downloadPokemons();
-    },[]);
+    },[pokedexUrl]);
 
     return(
           <div className="pokemon-list-wrapper">
@@ -47,8 +53,8 @@ function PokemonList(){
                 }
             </div>
             <div className="controls">
-                <button>Prev</button>
-                <button>Next</button>
+                <button disabled={prevUrl == null} onClick={()=>setPokedexUrl(prevUrl)}>Prev</button>
+                <button disabled={nextUrl == null} onClick={()=>setPokedexUrl(nextUrl)}>Next</button>
             </div>
           </div>
     )
